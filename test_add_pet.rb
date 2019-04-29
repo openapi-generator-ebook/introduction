3fe7e3ad8e52121cefe32915f84cba3a2ffdbd85
William Cheng <wing328hk@gmail.com>
2019-04-29 15:03:50 +0800

    updated with openapi-generator

--- test_add_pet.rb
+++ test_add_pet.rb
@@ -1,22 +1,22 @@
 # load the gem
-require 'swagger_client'
+require 'openapi_client'
 # setup authorization
-SwaggerClient.configure do |config|
+OpenapiClient.configure do |config|
   # Configure OAuth2 access token for authorization: petstore_auth
   config.access_token = 'ANYTHING_HERE_WILL_DO'
 end
 
-api_instance = SwaggerClient::PetApi.new
+api_instance = OpenapiClient::PetApi.new
 
-body = SwaggerClient::Pet.new # Pet | Pet object that needs to be added to the store
+body = OpenapiClient::Pet.new # Pet | Pet object that needs to be added to the store
 
 body.id = rand(10000..90000)
-body.name = "swagger-codegen-ebook-test"
+body.name = "openapi-generator-ebook-test"
 
 begin
   #Add a new pet to the store
   api_instance.add_pet(body)
   puts "Added a Pet with id " + body.id.to_s
-rescue SwaggerClient::ApiError => e
+rescue OpenapiClient::ApiError => e
   puts "Exception when calling PetApi->add_pet: #{e}"
 end
