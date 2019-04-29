8e2b9d1039487585bdf9ab0bcc2d27250ca9b16f
openapi-generator-ebook <50078672+openapi-generator-ebook@users.noreply.github.com>
2019-04-29 15:08:04 +0800

    updated with openapi-generator

--- test_get_pet_by_id.rb
+++ test_get_pet_by_id.rb
@@ -1,14 +1,14 @@
 # load the gem
-require 'swagger_client'
+require 'openapi_client'
 # setup authorization
-SwaggerClient.configure do |config|
+OpenapiClient.configure do |config|
   # Configure API key authorization: api_key
   config.api_key['api_key'] = 'ANYTHING_WILL_DO'
   # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
   #config.api_key_prefix['api_key'] = 'Bearer'
 end
 
-api_instance = SwaggerClient::PetApi.new
+api_instance = OpenapiClient::PetApi.new
 
 pet_id = 789 # Integer | ID of pet to return
 
@@ -22,6 +22,6 @@ begin
   #Find pet by ID
   result = api_instance.get_pet_by_id(pet_id)
   p result
-rescue SwaggerClient::ApiError => e
+rescue OpenapiClient::ApiError => e
   puts "Exception when calling PetApi->get_pet_by_id: #{e}"
 end
